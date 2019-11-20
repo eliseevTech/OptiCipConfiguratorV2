6ce749338aa52b1bbb7b65d173788ecd3e084980
Eliseev Eugene <eliseevmail@ya.ru>
2019-11-20 10:51:34 +0300

    Add project files.

---
+++ EntityAccessOnFramework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityAccessOnFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
